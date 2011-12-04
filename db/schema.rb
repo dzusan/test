3dbb28341210960ec4b3db11067dca88ec4f85e8
Sergey Lenkov <dzusan_creator@yahoo.com>
2011-12-04 22:52:11 +0400

    delete salt from db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111114193634) do
+ActiveRecord::Schema.define(:version => 20111114113006) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(:version => 20111114193634) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "salt"
   end
 
 end
