901ec57755b186a43bbca8825fcc54b1a103a5c6
Sergey Lenkov <dzusan_creator@yahoo.com>
2011-12-04 20:53:42 +0400

    type .css into application.rb

--- config/application.rb
+++ config/application.rb
@@ -50,5 +50,8 @@ module SiteProject
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Precompiling css.
+    config.assets.precompile += ['reset.css']
   end
 end
